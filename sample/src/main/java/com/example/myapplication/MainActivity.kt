ede3fd5182faf0cbff91723f202b1adb43f6dc44
aquilesTrindade <sempedra075@gmail.com>
2024-07-25 16:30:16 -0300

    fix: remember

--- sample/src/main/java/com/example/myapplication/MainActivity.kt
+++ sample/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,44 +6,43 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.text.style.TextOverflow
-
 import com.example.myapplication.ui.theme.MyComposeApplicationTheme
-
 import dev.trindadedev.lib.ui.components.preferences.compose.*
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val switchValue = by remember { mutableStateOf(false) }
         setContent {
             MyComposeApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                   Preference(
-                      iconResId = R.drawable.ic_settings_24,
-                      title = "Settings",
-                      summary = "Customize app with app settings",
-                      onClick = {
-                         
-                      }
-                   )
-                   PreferenceSwitch(
-                      iconResId = R.drawable.ic_settings_24,
-                      title = "Night mode",
-                      summary = "Active night mode for night",
-                      isSwitchChecked = switchValue,
-                      onSwitchCheckedChange = { isChecked ->
-                           switchValue = isChecked;
-                      }
-                   )
+                    var switchValue by remember { mutableStateOf(false) }
+
+                    Column {
+                        Preference(
+                            iconResId = R.drawable.ic_settings_24,
+                            title = "Settings",
+                            summary = "Customize app with app settings",
+                            onClick = {
+                                // Ação ao clicar no Preference
+                            }
+                        )
+
+                        PreferenceSwitch(
+                            iconResId = R.drawable.ic_settings_24,
+                            title = "Night mode",
+                            summary = "Activate night mode for night",
+                            isSwitchChecked = switchValue,
+                            onSwitchCheckedChange = { isChecked ->
+                                switchValue = isChecked
+                            }
+                        )
+                    }
                 }
             }
         }
