2b0bd5c59c2208252eac84c40b851c72c1cbf627
aquilesTrindade <sempedra075@gmail.com>
2024-07-25 15:48:34 -0300

    fix: preferences imports

--- sample/src/main/java/com/example/myapplication/MainActivity.kt
+++ sample/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,7 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+
 import com.example.myapplication.ui.theme.MyComposeApplicationTheme
+
+import dev.trindadedev.lib.ui.components.preferences.compose.*
 
 class MainActivity : ComponentActivity() {
 
