8535c5001964c118b8513ee176dd29decbd1ba2f
aquilesTrindade <sempedra075@gmail.com>
2024-07-25 16:24:12 -0300

    fixes

--- components-compose/src/main/java/dev/trindadedev/lib/ui/components/preferences/compose/Preference.kt
+++ components-compose/src/main/java/dev/trindadedev/lib/ui/components/preferences/compose/Preference.kt
@@ -23,7 +23,7 @@ fun Preference(
 ) {
     Row(
         modifier = Modifier
-            .wrapContentWidth(),
+            .wrapContentWidth()
             .clickable(onClick = onClick)
             .padding(horizontal = 24.dp, vertical = 20.dp),
         verticalAlignment = Alignment.CenterVertically,
