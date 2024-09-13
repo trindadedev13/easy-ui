8218c8fe903afb1304ea32ec8e20284702c2636a
Aquiles Trindade <devsuay@gmail.com>
2024-09-13 01:26:47 -0300

    Update build.gradle.kts

--- filepicker/build.gradle.kts
+++ filepicker/build.gradle.kts
@@ -1,15 +1,13 @@
-
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
-    id("com.google.devtools.ksp")
     id("maven-publish")
 }
 
 android {
     namespace = "dev.trindadedev.easyui.filepicker"
-    compileSdk = 34
+    compileSdk = 35
     
     defaultConfig {
         minSdk = 21
@@ -27,33 +25,33 @@ android {
     }
     
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_18
-        targetCompatibility = JavaVersion.VERSION_18
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     
     kotlinOptions {
-        jvmTarget = "18"
+        jvmTarget = "17"
     }
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions.jvmTarget = "18"
+    kotlinOptions.jvmTarget = "17"
 }
 
 dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0-alpha05")
     implementation("androidx.appcompat:appcompat:1.7.0")
 }
 
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
-            groupId = "dev.trindadedev.easyui.components"
-            artifactId = "components"
-            version = "0.0.1"
-
-            from(components.findByName("release") ?: components["java"])
+            groupId = "com.github.trindadadev13"
+            artifactId = "filepicker"
+            version  = "0.0.1"
+            
+            from(components.findByName("release"))
 
             pom {
                 name.set("Easy UI FilePicker")
